c553c374e07e4f695c02764d2ff76dc8d5494d08
Siarhei <h1_n1@mail.ru>
2022-07-20 12:44:54 +0300

    feat: add

--- test.js
+++ test.js
@@ -266,3 +266,20 @@ function isPowerOfTwo(n){
     return Number.isInteger(Math.log2(n));
 }
 --------------------------------*/
+
+let List = [{ '4': 'dog' }, { '2': 'took' }, { '3': 'his' }, { '-2': 'Vatsan' }, { '5': 'for' }, { '6': 'a' }, { '12': 'spin' }]
+
+function sentence(List) {
+   List.sort((a, b) => Object.keys(a)[0] - Object.keys(b)[0])
+   List.map(obj => Object.values(obj)[0]).join('')
+   console.log(List)
+   /*
+   .sort((a, b) => Object.keys(a)[0] – Object.keys(b)[0])
+
+   .map(obj => Object.values(obj)[0])
+
+   .join(' ')
+   */
+}
+
+sentence(List)
