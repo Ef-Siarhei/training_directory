b56d0065eb2f468b17b4ae718645343f4eb201d5
Siarhei <h1_n1@mail.ru>
2022-07-27 17:11:13 +0300

    feat: add morse

--- test.js
+++ test.js
@@ -434,3 +434,74 @@ function killer(obj, dead) {
 -------------------------*/
 
 
+
+/*--------------------
+const MORSE_TABLE = {
+   '.-': 'a',
+   '-...': 'b',
+   '-.-.': 'c',
+   '-..': 'd',
+   '.': 'e',
+   '..-.': 'f',
+   '--.': 'g',
+   '....': 'h',
+   '..': 'i',
+   '.---': 'j',
+   '-.-': 'k',
+   '.-..': 'l',
+   '--': 'm',
+   '-.': 'n',
+   '---': 'o',
+   '.--.': 'p',
+   '--.-': 'q',
+   '.-.': 'r',
+   '...': 's',
+   '-': 't',
+   '..-': 'u',
+   '...-': 'v',
+   '.--': 'w',
+   '-..-': 'x',
+   '-.--': 'y',
+   '--..': 'z',
+   '.----': '1',
+   '..---': '2',
+   '...--': '3',
+   '....-': '4',
+   '.....': '5',
+   '-....': '6',
+   '--...': '7',
+   '---..': '8',
+   '----.': '9',
+   '-----': '0',
+};
+
+const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+
+function decode(expr) {
+   let arrWordsBi = [];
+   let arrWordsMorse = [];
+   let space = '**********';
+   let result = "";
+
+   for (i = 0; i < expr.length; i += 10) {
+      arrWordsBi.push(expr.slice(i, i + 10));
+   }
+   for (i = 0; i < arrWordsBi.length; i++) {
+      let a = (arrWordsBi[i].match(/.{1,2}/g).join(''))
+         .replace(/10/g, '.').replace(/11/g, '-').replace(/00/g, '')
+      arrWordsMorse.push(a)
+   }
+
+   let keys = Object.keys(MORSE_TABLE)
+
+   arrWordsMorse.forEach(item => {
+      for (let i = 0; i < keys.length; i++) {
+         if (item === keys[i]) result += MORSE_TABLE[keys[i]];
+      }
+      if (item === space) result += ' ';
+   })
+
+   console.log(result)
+}
+decode(expr)
+-----------------------*/
